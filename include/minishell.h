a9c48ee93dff169752286bee506fc1a3d2a9779d
Daniel Jiménez <danielji@student.42madrid.com>
2025-09-22 09:49:25 +0200

    Include stdlib, stdio, readline

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: danielji <danielji@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/22 09:18:26 by danielji          #+#    #+#             */
-/*   Updated: 2025/09/22 09:29:45 by danielji         ###   ########.fr       */
+/*   Updated: 2025/09/22 09:40:52 by danielji         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -14,5 +14,15 @@
 # define MINISHELL_H
 
 # include "libft.h"
+
+// malloc, free
+# include <stdlib.h>
+
+// readline
+# include <stdio.h>
+
+// readline
+# include <readline/readline.h>
+# include <readline/history.h>
 
 #endif
