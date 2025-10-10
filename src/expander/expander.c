cefa27f3afd994e4921bef875a4b65cd67108851
Enrique Gil Piqueras <enrgil-p@c2r1s1.42madrid.com>
2025-10-10 20:12:24 +0200

    FOR LAST COMMIT: I did not added strchr_and_position on the Makefile. What I am doing know is to get the new len with an easy way. Old chars len that I will use will be all - the dollar and the name of env var to expand. Continue tomorrow...

--- src/expander/expander.c
+++ src/expander/expander.c
@@ -2,7 +2,7 @@
 
 //header file.h here
 
-int	count_new_len(char *str, int *dollar_position)
+int	count_new_len(char *str, char **name_to_expand, int dollar_position)
 {
 	int	i;
 	int	new_len;
@@ -13,13 +13,13 @@ int	count_new_len(char *str, int *dollar_position)
 	expansion_flag = 0;
 	while (str[i])
 	{
+		//CREATE A STRLEN which works looking for $
+		//Or maybe obtain the position with a modified strchr
 		if (!expansion_flag && word->value[i] == DOLLAR)
 		{
 			new_len = i;
-			*dollar_position = i;
 			expansion_flag = 1;
-			while (/*end of $VAR*/)
-				++i;
+
 		}
 		++i;
 		if (expansion_flag)
@@ -34,17 +34,17 @@ int	count_new_len(char *str, int *dollar_position)
 {
 	int	new_len;
 	int	dollar_position;
+	char	*name_to_expand;
 	char	*enviroment_expanded;
 	char	*new_value;
 
 	dollar_position = 0;
-	while (ft_strchr(word, DOLLAR))
+	while (ft_strchr_and_position(word, DOLLAR, dollar_position))
 	{
-		new_len = count_new_len(word->value, &dollar_position);
-		//What string we put to getenv? Buddy... Make a struct,
-		//and get in count_new_len the whole name of env
-		enviroment_expanded = getenv();
-		/*struct->env_len*/ = ft_strlen(enviroment_expanded);
+		new_len = count_new_len(word->value,
+			&name_to_expand, dollar_position);
+		enviroment_expanded = getenv(name_to_expand);
+		new_len += ft_strlen(enviroment_expanded);
 		new_value = //function that do three memcopies
 		free(word->value);
 		word->value = new_value;
