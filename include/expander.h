4dacc784e22079d1ffc2e2e8046a370af1ab37e5
Enrique Gil Piqueras <enrgil-p@c2r2s1.42madrid.com>
2025-10-13 19:54:30 +0200

    Added resize_expansions() to expander.h, also added some new data to struct

--- include/expander.h
+++ include/expander.h
@@ -6,7 +6,7 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/12 17:23:08 by enrgil-p          #+#    #+#             */
-/*   Updated: 2025/10/12 20:33:00 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2025/10/13 19:52:41 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,12 @@ typedef struct s_expansion_data
 	int		resize_len;
 	int		var_name_len;
 	int		third_start;
+	int		expanded_len;
 	char	*expanded;
 }	t_expansion_data;
 
 int	find_expansion_and_get_data(const char *str,
 		t_expansion_data *exp_data);
+char	*resize_expansions(char *old_str, t_expansion_data *ed);
 
 #endif
