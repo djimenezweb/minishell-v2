f220bd03a665dbb712ea4d846e997cdecb5497cf
Enrique Gil Piqueras <enrgil-p@c2r3s2.42madrid.com>
2025-10-14 20:09:01 +0200

    Added in expander.h just the few functions that were needed to put there

--- include/expander.h
+++ include/expander.h
@@ -6,7 +6,7 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/12 17:23:08 by enrgil-p          #+#    #+#             */
-/*   Updated: 2025/10/13 19:52:41 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2025/10/14 20:06:44 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,14 @@ typedef struct s_expansion_data
 	int		dollar_position;
 	int		resize_len;
 	int		var_name_len;
-	int		third_start;
 	int		expanded_len;
 	char	*expanded;
 }	t_expansion_data;
 
+void	check_token_words(t_lextoken **token_list);
 int	find_expansion_and_get_data(const char *str,
 		t_expansion_data *exp_data);
+char		*get_variable_name(char *str);
 char	*resize_expansions(char *old_str, t_expansion_data *ed);
 
 #endif
