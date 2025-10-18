543dca97938a345f95c3963d91a844190033ed91
Enrique Gil Piqueras <enrgil-p@c2r17s3.42madrid.com>
2025-10-18 23:18:35 +0200

    Cleaning for norminette. Not completed, delete any comment not needed

--- src/expander/expander.c
+++ src/expander/expander.c
@@ -6,7 +6,7 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/12 19:52:08 by enrgil-p          #+#    #+#             */
-/*   Updated: 2025/10/18 22:20:08 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2025/10/18 23:17:02 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,20 +25,20 @@ static void	update_expansion_data(t_expansion_data *exp_data)
 	exp_data->expanded = NULL;
 }
 
-static void	manage_expansions_and_quotes(t_lextoken **word) 
+static void	manage_expansions_and_quotes(t_lextoken **word)
 //This may work with a TOK_WORD, so it's not needed to change
 //ENRIQUE-12/10/2025: RETURN VALUE must be to be sure if some malloc fails
 //ENRIQUE-14/10/2025: Maybe just not return, and in case of malloc fail,
 //use an struct and free all at the scope where the fail has happened
 {
-	char	*new_value;
 	t_expansion_data	exp_data;
+	char				*new_value;
 
 	update_expansion_data(&exp_data);
 	while (find_expansion_and_get_data((*word)->value, &exp_data))
 	{
 		exp_data.expanded = getenv(exp_data.var_name);
-		new_value = resize_expansions((*word)->value, &exp_data); 
+		new_value = resize_expansions((*word)->value, &exp_data);
 		free((*word)->value);
 		(*word)->value = new_value;
 		update_expansion_data(&exp_data);
