3a0cfe502a978501914b6de8d80b1e52abc6267d
Daniel Jiménez <danielji@student.42madrid.com>
2025-10-21 10:59:08 +0200

    Add prev reference to list & modified comments

--- src/lexer/lexer-utils.c
+++ src/lexer/lexer-utils.c
@@ -12,7 +12,7 @@
 
 #include "minishell.h"
 
-/* Adds the node `new` after the node `prev` */
+/* Add the node `new` after the node `prev` */
 void	ft_lexlist_insert(t_lextoken *prev, t_lextoken *new)
 {
 	t_lextoken	*temp;
@@ -20,9 +20,10 @@ void	ft_lexlist_insert(t_lextoken *prev, t_lextoken *new)
 	temp = prev->next;
 	prev->next = new;
 	new->next = temp;
+	new->next->prev = new;
 }
 
-/* Adds the node `new` at the beginning of the list `lst`
+/* Add the node `new` at the beginning of the list `lst`
 if it's empty or at the end of the list */
 void	ft_lexlist_add(t_lextoken **lst, t_lextoken *new)
 {
@@ -36,11 +37,12 @@ void	ft_lexlist_add(t_lextoken **lst, t_lextoken *new)
 		while (last->next)
 			last = last->next;
 		last->next = new;
+		new->prev = last;
 	}
 }
 
-/* Takes a node as parameter and frees its content.
-Frees the node itself but does NOT free the next node. */
+/* Take a node as parameter and free its content.
+Free the node but do NOT free the next node. */
 void	ft_lexnode_free(t_lextoken *node)
 {
 	if (node->value)
@@ -52,7 +54,7 @@ void	ft_lexnode_free(t_lextoken *node)
 	node = NULL;
 }
 
-/* Deletes and frees the given node and all its successors.
+/* Delete and free the given node and all its successors.
 Finally, set the pointer to the list to `NULL`.*/
 void	ft_lexlist_clear(t_lextoken **lst)
 {
