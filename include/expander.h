f0f4f43fe243d2a8475313b48d57780fa7a49779
Enrique Gil Piqueras <enrgil-p@c2r11s2.42madrid.com>
2025-10-22 20:07:21 +0200

    Maintain the typedef struct s_shell t_shell, or create a structs.h. One of them must be chosen, soon

--- include/expander.h
+++ include/expander.h
@@ -6,7 +6,7 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/12 17:23:08 by enrgil-p          #+#    #+#             */
-/*   Updated: 2025/10/22 18:40:09 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2025/10/22 20:05:48 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,11 @@
 
 # include "minishell.h"
 
+
+typedef struct s_shell t_shell;//ENRIQUE 22/10: We shoudl create a structs.h
 typedef struct s_expansion_data
 {
+	//WATCH PUT, COMMENTS ABOVE
 	int		dollar_position;
 	int		resize_len;
 	int		var_name_len;
@@ -27,7 +30,7 @@ typedef struct s_expansion_data
 	char	*expanded;
 }	t_expansion_data;
 
-void	check_token_words(t_lextoken **token_list);
+void	expander(t_shell *data);
 int		find_expansion(char *str, t_expansion_data *exp_data);
 char	*get_variable_name(char *str);
 char	*resize_expansions(char *old_str, t_expansion_data *ed);
