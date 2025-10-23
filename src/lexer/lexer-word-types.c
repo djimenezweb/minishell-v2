83c9be1cd56628ca4028b67f08df2a49afbfb0bc
Daniel Jiménez <danielji@student.42madrid.com>
2025-10-23 13:02:15 +0200

    Fixed Norminette typos

--- src/lexer/lexer-word-types.c
+++ src/lexer/lexer-word-types.c
@@ -22,13 +22,15 @@ void	parse_word_types(t_lextoken *lst)
 		{
 			if (!lst->prev)
 				lst->word_type = TOK_COMMAND;
-			else if (lst->prev->type == TOK_REDIR_OUT || lst->prev->type == TOK_APPEND)
+			else if (lst->prev->type == TOK_REDIR_OUT
+				|| lst->prev->type == TOK_APPEND)
 				lst->word_type = TOK_OUTFILE;
 			else if (lst->prev->type == TOK_REDIR_IN)
 				lst->word_type = TOK_INFILE;
 			else if (lst->prev->type == TOK_HEREDOC)
 				lst->word_type = TOK_DELIMITER;
-			else if (lst->prev->word_type == TOK_COMMAND || lst->prev->word_type == TOK_ARGUMENT)
+			else if (lst->prev->word_type == TOK_COMMAND
+				|| lst->prev->word_type == TOK_ARGUMENT)
 				lst->word_type = TOK_ARGUMENT;
 			else
 				lst->word_type = TOK_COMMAND;
