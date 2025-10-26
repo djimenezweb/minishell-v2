86abe6ed32172afb34d8967483010e622c0c0f89
Enrique Gil Piqueras <enrgil-p@c2r11s2.42madrid.com>
2025-10-26 19:00:48 +0100

    Created file for exit builtin, let start thinking of how do this

---
+++ src/builtin/exit.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exit.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: enrgil-p <enrgil-p@student.42madrid.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/26 18:52:23 by enrgil-p          #+#    #+#             */
+/*   Updated: 2025/10/26 19:00:12 by enrgil-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include 'SOMETHING'//TO DO
+
+//	PURPOSE
+/*exit command must be read a word exit, and execute a free of memory
+ * and close the execution*/
+
+static void	ft_exit(t_shell	*data)
+{
+}
