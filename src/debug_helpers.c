a2f8a3bb9c9f135b77932acd9fb127d38dd77b9e
djimenezweb <djimenezweb@gmail.com>
2025-11-01 17:23:05 +0100

    Add color to debug helper

--- src/debug_helpers.c
+++ src/debug_helpers.c
@@ -3,7 +3,75 @@
 
 #include "minishell.h"
 
+# define RESET "\033[0m"
+# define BLACK "\033[0;30m"
+# define RED "\033[0;31m"
+# define GREEN "\033[0;32m"
+# define YELLOW "\033[0;33m"
+# define BLUE "\033[0;34m"
+# define MAGENTA "\033[0;35m"
+# define CYAN "\033[0;36m"
+# define WHITE "\033[0;37m"
+
+# define BBLACK "\033[1;30m"
+# define BRED "\033[1;31m"
+# define BGREEN "\033[1;32m"
+# define BYELLOW "\033[1;33m"
+# define BBLUE "\033[1;34m"
+# define BMAGENTA "\033[1;35m"
+# define BCYAN "\033[1;36m"
+# define BWHITE "\033[1;37m"
+
+# define BG_RED "\033[41m"
+# define BG_GREEN "\033[42m"
+# define BG_YELLOW "\033[43m"
+# define BG_BLUE "\033[44m"
+# define BG_MAGENTA "\033[45m"
+# define BG_CYAN "\033[46m"
+# define BG_WHITE "\033[47m"
+
+
 void	print_lex_list(t_lextoken *list)
+{
+	t_lextoken	*curr;
+
+	curr = list;
+	while (curr)
+	{
+		if (curr->type == TOK_WORD)
+		{
+			if (curr->word_type >= 0)
+			{
+				if (curr->word_type == TOK_COMMAND)
+					printf(BGREEN"%s "RESET, curr->value);
+				if (curr->word_type == TOK_ARGUMENT)
+					printf(CYAN"%s "RESET, curr->value);
+				if (curr->word_type == TOK_INFILE)
+					printf(BG_CYAN"%s"RESET" ", curr->value);
+				if (curr->word_type == TOK_OUTFILE)
+					printf(BG_MAGENTA"%s"RESET" ", curr->value);
+				if (curr->word_type == TOK_DELIMITER)
+					printf(BG_YELLOW"%s"RESET" ", curr->value);
+			}
+		}
+		else if (curr->type == TOK_PIPE)
+			printf(BYELLOW"| "RESET);
+		else if (curr->type == TOK_REDIR_IN)
+			printf(BG_CYAN"<"RESET);
+		else if (curr->type == TOK_REDIR_OUT)
+			printf(BG_MAGENTA">"RESET);
+		else if (curr->type == TOK_APPEND)
+			printf(BG_MAGENTA">>"RESET);
+		else if (curr->type == TOK_HEREDOC)
+			printf(BG_YELLOW"<<"RESET);
+		else if (curr->type == TOK_EOF)
+			printf(BG_WHITE"EOF"RESET);
+		curr = curr->next;
+	}
+	printf("\n");
+}
+
+/* void	print_lex_list(t_lextoken *list)
 {
 	t_lextoken	*curr;
 
@@ -42,7 +110,7 @@ void	print_lex_list(t_lextoken *list)
 			ft_printf("EOF\n");
 		curr = curr->next;
 	}
-}
+} */
 
 void	print_array_of_strings(char **arr)
 {
