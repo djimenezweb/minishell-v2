16831a61f24055297e8a643e18389c45484791b4
Enrique Gil Piqueras <enrgil-p@c2r15s1.42madrid.com>
2025-11-11 18:16:20 +0100

    Now we don't need to mention some other struct in expander.h

--- include/expander.h
+++ include/expander.h
@@ -6,7 +6,7 @@
 /*   By: danielji <danielji@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/12 17:23:08 by enrgil-p          #+#    #+#             */
-/*   Updated: 2025/11/10 13:32:17 by danielji         ###   ########.fr       */
+/*   Updated: 2025/11/11 18:12:19 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -15,13 +15,9 @@
 
 # include "minishell.h"
 
-//ENRIQUE 22/10: We shoudl create a structs.h
-
-typedef struct s_shell	t_shell;
 
 typedef struct s_expansion_data
 {
-	//WATCH PUT, COMMENTS ABOVE
 	int		dollar_position;
 	int		resize_len;
 	int		var_name_len;
