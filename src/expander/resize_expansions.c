3c83a56e5aad4095fad26567e074b83eb1604e24
Enrique Gil Piqueras <enrgil-p@c2r2s6.42madrid.com>
2025-11-16 16:12:52 +0100

    Problem was with resize_expansion. Now it's corrected

--- src/expander/resize_expansions.c
+++ src/expander/resize_expansions.c
@@ -6,7 +6,7 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 19:16:07 by enrgil-p          #+#    #+#             */
-/*   Updated: 2025/11/12 20:34:12 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2025/11/16 16:07:15 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,6 @@ static void	init_joined_vars(t_join *to_init)
 	to_init->dst = NULL;
 	to_init->src = NULL;
 	to_init->len = 0;
-}
-
-static void	set_len_in_loop_3(t_join *j, t_expansion_data *ed)
-{
-	if (j->dst[0] == '\0')
-		j->len = ft_strlen(j->src) + 1;
-	else
-		j->len = ed->resize_len - (ed->dollar_position
-				+ ed->var_name_len);
 }
 
 static void	join_expansion(char *old_str, char **new_str,
@@ -52,9 +43,9 @@ static void	join_expansion(char *old_str, char **new_str,
 		joined.src = old_str + (ed->dollar_position + ed->var_name_len);
 		joined.dst = *new_str + (ed->dollar_position
 				+ ed->expanded_len);
-		set_len_in_loop_3(&joined, ed);
+		if (joined.src[0] != '\0')
+			joined.len = ft_strlen(joined.src) + 1;
 	}
-	printf("src %s, dst %s, len %d\n", joined.src, joined.dst, joined.len);//debug
 	if (joined.len != 0)
 		ft_memcpy(joined.dst, joined.src, joined.len);
 }
@@ -67,7 +58,7 @@ char	*resize_expansions(char *old_str, t_expansion_data *ed)
 	if (ed->expanded)
 		ed->expanded_len = ft_strlen(ed->expanded);
 	if (ed->expanded_len != 0)
-		ed->resize_len += (ed->expanded_len - 1);
+		ed->resize_len += ed->expanded_len;
 	new_str = ft_calloc((ed->resize_len + 1), sizeof(char));
 	if (!new_str)
 		return (NULL);
