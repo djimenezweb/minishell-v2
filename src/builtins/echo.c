bdfd8c71edba70275a0e73f389fec44cd06dc795
enrique_gil_piqueras (enrgil-p) <131750246+KikeEGP@users.noreply.github.com>
2025-11-18 23:14:50 +0100

    Working on echo at home. No tabs, norminette must be cleaned

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -26,7 +26,36 @@ static int	is_option_n(char *arg_one)
 	return (1);
 }
 
-/*Some*/	execute_echo(t_cmd *cmd/*, int pipefd[2]??*/)
+int      count_chars_from(char **args)
+{
+	int   count;
+
+	count = 0;
+	while (*args)
+	{
+		count = ft_strlen(*args);
+		if (*(args + 1))
+			count += 1;
+		++args;
+	}
+	return (count);
+}
+
+int   join_to_write(char **args, int len, int option_n, char **joined)
+{
+	char *iterator;
+	*joined = ft_calloc(len, sizeof(char));
+	if (!(*joined))
+		return (0);
+	iterator = *joined;
+	while (args)
+	{
+	}
+	return (1);
+}
+	
+//Join args to write it at once
+int	execute_echo(t_cmd *cmd/*, int pipefd[2]??*/)
 {
 	int	write_len;
 	int	option_n;
@@ -45,11 +74,8 @@ static int	is_option_n(char *arg_one)
 		/*Concatenate all the options, with 32 space. If not -n, 
 		 * add a \n. at the end*/
 	}
-	//count_args()
-	//if count is 1, add or not '\n', and write it
-	//if count >= 2, join arg + ' ' + arg, until it is the last
-	//Or maybe create a function that copies inside joined_to_write
-	//	arg by arg, add ' ' if current arg is != count_args,
-	//	and add later \n if it is needed
-	write_len += ft_strlen(joined_to_write);
+	write_len += count_chars_from(args);
+	if (!join_to_write(args, write_len, option_n, &joined_to_write))
+		return (0);//malloc error happened
+	write(/*output_fd*/, joined_to_write, write_len);
 }
