337cbcbed0f9b2b87d3134e2dd5c4f594cef4f76
Daniel Jiménez <danielji@student.42madrid.com>
2025-11-18 11:58:40 +0100

    Fixed norminette errors

--- src/execution/heredoc.c
+++ src/execution/heredoc.c
@@ -12,14 +12,8 @@
 
 #include "minishell.h"
 
-int	init_heredoc(int *is_quoted, char *delim, int here_pipe[2])
+static int	init_heredoc(int here_pipe[2])
 {
-	*is_quoted = 0;
-	if (ft_strchr(delim, DOUBLE_QUOTE))
-	{
-		*is_quoted = 1;
-		remove_quotes(delim);
-	}
 	if (here_pipe[READ_END] != -1)
 		safe_close(here_pipe[READ_END]);
 	if (here_pipe[WRITE_END] != -1)
@@ -29,7 +23,7 @@ int	init_heredoc(int *is_quoted, char *delim, int here_pipe[2])
 	return (0);
 }
 
-int	is_delimiter(char *delim, char *line)
+static int	is_delimiter(char *delim, char *line)
 {
 	size_t	len;
 
@@ -41,44 +35,49 @@ int	is_delimiter(char *delim, char *line)
 	return (0);
 }
 
+static void	heredoc_loop(t_cmd *cmd, int i, int here_pipe[2])
+{
+	int		is_quoted;
+	char	*line;
+
+	is_quoted = 0;
+	if (ft_strchr(cmd->delimiters[i], DOUBLE_QUOTE))
+	{
+		is_quoted = 1;
+		remove_quotes(cmd->delimiters[i]);
+	}
+	line = readline("> ");
+	while (line)
+	{
+		if (is_delimiter(cmd->delimiters[i], line))
+		{
+			free(line);
+			break ;
+		}
+		if (!is_quoted && ft_strchr(line, DOLLAR))
+			expander(&line, cmd->env_list);
+		ft_putendl_fd(line, here_pipe[WRITE_END]);
+		free(line);
+		line = readline("> ");
+	}
+}
+
 int	heredoc(t_cmd *cmd)
 {
-	int		i;
-	int		is_quoted;
-	int		here_pipe[2];
-	char	*line;
+	int	i;
+	int	here_pipe[2];
 
 	i = 0;
 	here_pipe[READ_END] = -1;
 	here_pipe[WRITE_END] = -1;
 	while (cmd->delimiters[i])
 	{
-		if (init_heredoc(&is_quoted, cmd->delimiters[i], here_pipe) < 0)
+		if (init_heredoc(here_pipe) < 0)
 			return (-1);
-		while (1)
-		{
-			line = readline("> ");
-			if (!line)
-				break ;
-			else if (is_delimiter(cmd->delimiters[i], line))
-			{
-				free(line);
-				break ;
-			}
-			if (!is_quoted && ft_strchr(line, DOLLAR))
-				expander(&line, cmd->env_list);
-			ft_putendl_fd(line, here_pipe[WRITE_END]);
-			free(line);
-		}
+		heredoc_loop(cmd, i, here_pipe);
 		i++;
 	}
 	safe_close(here_pipe[WRITE_END]);
 	cmd->input = here_pipe[READ_END];
 	return (0);
 }
-
-/* 
-
-
-
-*/
