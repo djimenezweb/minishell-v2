6193a8c8600867a7fd5539c83e89e4529cee30b9
Daniel Jiménez <danielji@student.42madrid.com>
2025-11-20 16:50:21 +0100

    add header guards

--- include/builtins.h
+++ include/builtins.h
@@ -6,12 +6,17 @@
 /*   By: danielji <danielji@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/19 10:53:51 by danielji          #+#    #+#             */
-/*   Updated: 2025/11/20 16:13:06 by danielji         ###   ########.fr       */
+/*   Updated: 2025/11/20 16:49:57 by danielji         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
-#include "minishell.h"
+#ifndef BUILTINS_H
+# define BUILTINS_H
+
+# include "minishell.h"
 
 int	ft_exit(void);
 //int	ft_exit(t_shell shell);
 int	execute_echo(t_cmd *cmd);
+
+#endif
