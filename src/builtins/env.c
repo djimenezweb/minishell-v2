226cb9eb4a7e17d399b99538b0bd550a75b3560b
Enrique Gil Piqueras <enrgil-p@c2r4s1.42madrid.com>
2025-11-23 14:41:15 +0100

    env. I forgot to protect malloc error and free() after write(). Now it's done. Needs testing yet

--- src/builtins/env.c
+++ src/builtins/env.c
@@ -6,16 +6,11 @@
 /*   By: danielji <danielji@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/22 12:48:00 by danielji          #+#    #+#             */
-/*   Updated: 2025/11/23 02:54:33 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2025/11/23 14:39:50 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-/* Todos los builtins devuelven 0 si han tenido éxito o un número distinto de
-0 en caso de error.
-Devuelven 2 para indicar uso incorrecto, opciones inválidas o que faltan
-argumentos. */
 
 int	ft_env(char **envp)
 {
@@ -25,11 +20,18 @@ int	ft_env(char **envp)
 
 	i = 0;
 	len = 0;
+	write_str = NULL;
 	while (envp[i] != NULL)
 	{
 		write_str= ft_strjoin(envp[i++], "\n");
+		if (!write_str)
+		{
+			perror ("echo failed in malloc");
+			return (12);
+		}
 		len = ft_strlen(write_str);
 		write(STDOUT_FILENO, write_str, len);
+		free(write_str);
 	}
 //	ft_putendl_fd("env test", STDOUT_FILENO);
 	return (0);
