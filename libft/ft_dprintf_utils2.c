03908adf192eb706e7d58d53674b6dd3e7240417
djimenezweb <djimenezweb@gmail.com>
2025-11-24 15:22:57 +0100

    Restructured ft_printf & ft_dprinft files

--- libft/ft_dprintf_utils2.c
+++ libft/ft_dprintf_utils2.c
@@ -1,21 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_puthex_fd_utils.c                               :+:      :+:    :+:   */
+/*   ft_dprintf_utils2.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: danielji <danielji@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/11 22:57:16 by danielji          #+#    #+#             */
-/*   Updated: 2025/11/24 13:24:57 by danielji         ###   ########.fr       */
+/*   Updated: 2025/11/24 15:21:02 by danielji         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/* Writes a number as hexadecimal.
+/* Write a number as hexadecimal to the specified file descriptor `fd`.
 `uintptr_t` is an `unsigned int` type designed to hold pointer values.
 Base is passed as a string: `"0123456789abcdef"` or `"0123456789ABCDEF"`
-Returns number of characters written*/
+Return number of characters written */
 static ssize_t	ft_dputhex_r(int fd, uintptr_t num, char *base)
 {
 	char	c;
@@ -29,15 +29,15 @@ static ssize_t	ft_dputhex_r(int fd, uintptr_t num, char *base)
 	return (written);
 }
 
-/* Writes an unsigned int number as hexadecimal.
-Returns number of characters written*/
+/* Write an unsigned int number as hexadecimal to the specified file
+descriptor `fd` and return the number of characters written */
 ssize_t	ft_dputhex_ir(int fd, unsigned int num, char *base)
 {
 	return (ft_dputhex_r(fd, (uintptr_t)num, base));
 }
 
-/* Writes a pointer by calling ft_puthex_r or writing 0.
-Returns number of characters written */
+/* Write a pointer by calling `ft_dputhex_r` or writing `(nil)`.
+Return the number of characters written */
 ssize_t	ft_dputptr_r(int fd, void *ptr)
 {
 	ssize_t		written;
--- libft/ft_printf_utils1.c
+++ libft/ft_printf_utils1.c
@@ -1,16 +1,57 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putnbr_r.c                                      :+:      :+:    :+:   */
+/*   ft_printf_utils1.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: danielji <danielji@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/19 16:23:30 by danielji          #+#    #+#             */
-/*   Updated: 2025/05/19 16:24:59 by danielji         ###   ########.fr       */
+/*   Created: 2025/05/19 16:18:29 by danielji          #+#    #+#             */
+/*   Updated: 2025/11/24 15:13:29 by danielji         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/* Outputs the character `c` and returns number of characters written */
+ssize_t	ft_putchar_r(char c)
+{
+	ssize_t	written;
+
+	written = write(1, &c, 1);
+	return (written);
+}
+
+/* Outputs the string `s` and returns number of characters written.*/
+ssize_t	ft_putstr_r(char *s)
+{
+	ssize_t	written;
+
+	written = 0;
+	if (!s)
+		written += write(1, "(null)", 6);
+	while (s && *s)
+	{
+		written += write(1, s, 1);
+		s++;
+	}
+	return (written);
+}
+
+/* Outputs the unsigned integer `n` and returns number of characters written */
+ssize_t	ft_putnbr_u_r(unsigned int n)
+{
+	char	c;
+	ssize_t	written;
+
+	written = 0;
+	if (n / 10 > 0)
+	{
+		written += ft_putnbr_u_r(n / 10);
+	}
+	c = (n % 10) + '0';
+	written += write(1, &c, 1);
+	return (written);
+}
 
 /* Outputs the integer `n` and returns number of characters written */
 ssize_t	ft_putnbr_r(int n)
