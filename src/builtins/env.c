9e7bb57b0929e789a3355cfb7e973ba70ca898ef
Enrique Gil Piqueras <enrgil-p@c2r13s1.42madrid.com>
2025-11-25 20:05:49 +0100

    env. I forgot to not print ?. Now it's really protected all

--- src/builtins/env.c
+++ src/builtins/env.c
@@ -6,7 +6,7 @@
 /*   By: danielji <danielji@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/22 12:48:00 by danielji          #+#    #+#             */
-/*   Updated: 2025/11/25 19:45:50 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2025/11/25 20:04:47 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,9 @@ int	ft_env(char **cmd, char **envp)
 {
 	int		i;
 	int		len;
-	char	*write_str;
 
 	i = 0;
 	len = 0;
-	write_str = NULL;
 	if (cmd[1])
 	{
 		ft_putendl_fd("env: expected no arguments", 2);
@@ -28,15 +26,9 @@ int	ft_env(char **cmd, char **envp)
 	}
 	while (envp[i] != NULL)
 	{
-		write_str = ft_strjoin(envp[i++], "\n");
-		if (!write_str)
-		{
-			perror ("echo failed in malloc");
-			return (12);
-		}
-		len = ft_strlen(write_str);
-		write(STDOUT_FILENO, write_str, len);
-		free(write_str);
+		if (envp[i][0] != '?')
+			ft_putendl_fd(envp[i], STDOUT_FILENO);
+		++i;
 	}
 	return (0);
 }
