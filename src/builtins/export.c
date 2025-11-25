31117275c51c07bff12a55ce229bf904443ebda7
Enrique Gil Piqueras <enrgil-p@c2r13s1.42madrid.com>
2025-11-25 20:51:27 +0100

    First version of export works. Things to do: if not = called in the export, just store name of var, with no dollar. Or not. It's something useful. And think abut sort or not sort the export. pdel-olm and daxferna didn't sort their export

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -6,19 +6,43 @@
 /*   By: danielji <danielji@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/22 12:47:48 by danielji          #+#    #+#             */
-/*   Updated: 2025/11/24 23:15:29 by danielji         ###   ########.fr       */
+/*   Updated: 2025/11/25 20:48:35 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-/* Todos los builtins devuelven 0 si han tenido éxito o un número distinto de
-0 en caso de error.
-Devuelven 2 para indicar uso incorrecto, opciones inválidas o que faltan
-argumentos. */
-
-int	ft_export(void)
+static void	create_new_env_vars(char *input, t_env_var *env_list, int *flag)
 {
-	ft_putendl_fd("export test", STDOUT_FILENO);
-	return (0);
+	t_env_var	*new_node;
+
+	new_node = NULL;
+	new_node = ft_new_env(input);
+	if (!new_node)
+		*flag = 1;
+	ft_env_addback(&env_list, new_node);
+	//if (!ft_strchr(input, '='))
+		//remove_equal();TODO
+}
+
+int	ft_export(char **cmd, t_env_var *env_list, char **envp)
+{
+	int	error_flag;
+
+	error_flag = 0;
+	if (!cmd[1])
+		return (ft_env(cmd, envp));//debug. Here we should sort env vars
+	else
+	{
+		cmd += 1;
+		while (*cmd && !error_flag)
+		{
+			create_new_env_vars(*cmd, env_list, &error_flag);
+			//Check if env_list is modified
+			++cmd;
+		}
+		if  (error_flag)
+			ft_putendl_fd("env: failed creating new env_var", 2);
+	}
+	return (error_flag);
 }
--- src/execution/call_to_builtins.c
+++ src/execution/call_to_builtins.c
@@ -6,7 +6,7 @@
 /*   By: danielji <danielji@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/18 20:23:43 by enrgil-p          #+#    #+#             */
-/*   Updated: 2025/11/25 19:40:14 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2025/11/25 20:01:04 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ int	call_to_builtins(t_cmd *cmd, char **envp, t_env_var *env_list)
 		else if (cmd->cmd[0][1] == 'n')
 			return (ft_env(cmd->cmd, envp));
 		else if (cmd->cmd[0][2] == 'p')
-			return (ft_export());//needs env_list
+			return (ft_export(cmd->cmd, env_list, envp));//needs env_list
 		else if (cmd->cmd[0][2] == 'i')
 			return (ft_exit());
 	}
