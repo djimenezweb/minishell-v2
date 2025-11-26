03f3b366acd95acfe0cca0e86d0dc6227ab3c6a1
Enrique Gil Piqueras <enrgil-p@c2r7s1.42madrid.com>
2025-11-26 20:41:32 +0100

    Removed a comment

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -6,7 +6,7 @@
 /*   By: danielji <danielji@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/22 12:47:48 by danielji          #+#    #+#             */
-/*   Updated: 2025/11/26 20:20:38 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2025/11/26 20:40:26 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,6 @@ static void	create_new_env_vars(char *input, t_env_var *env_list, int *flag)
 		ft_env_addback(&env_list, new_node);
 	else
 		change_env_value(already_exist_env_var, new_node->value);
-	//if (!ft_strchr(input, '='))
-		//remove_equal();TODO
 }
 
 int	ft_export(char **cmd, t_env_var *env_list, char **envp)
