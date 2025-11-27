5e1d261f6cc8a3737f89f17b60ac0a39328befd3
Daniel Jiménez <danielji@student.42madrid.com>
2025-11-27 18:31:15 +0100

    is_first & is_last auxiliary functions

--- include/syntax.h
+++ include/syntax.h
@@ -6,7 +6,7 @@
 /*   By: danielji <danielji@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 10:38:39 by danielji          #+#    #+#             */
-/*   Updated: 2025/10/21 11:29:31 by danielji         ###   ########.fr       */
+/*   Updated: 2025/11/27 18:08:17 by danielji         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,7 @@ int		is_valid_redout(t_lextoken *node);
 int		is_valid_pipe(t_lextoken *node);
 void	print_syntax_error(char *str);
 int		syntax_validation(t_lextoken *node);
+int		is_tok_last(t_lextoken *node);
+int		is_tok_first(t_lextoken *node);
 
 #endif
