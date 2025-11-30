0c03c4a0a2f6b6edfbb31a53f7f9378ffad07646
Enrique Gil Piqueras <enrgil-p@c2r3s6.42madrid.com>
2025-11-30 18:01:07 +0100

    export, as happened with cd, caused leaks if it was called with pipes. Corrected, no doesn't work in case of pipes, as bash does, so there are no leaks

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -6,7 +6,7 @@
 /*   By: danielji <danielji@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/22 12:47:48 by danielji          #+#    #+#             */
-/*   Updated: 2025/11/29 22:58:02 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2025/11/30 17:57:35 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,7 +71,7 @@ int	ft_export(char **cmd, t_env_var *env_list, char **envp)
 	error_flag = 0;
 	if (!cmd[1])
 		return (ft_env(cmd, envp, 1));//debug. Here we should sort env vars
-	else
+	else if (env_list)
 	{
 		cmd += 1;
 		while (*cmd && error_flag != 2)
