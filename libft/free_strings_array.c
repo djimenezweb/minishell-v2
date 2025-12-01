5c5ec68a3e44566faeeb062e85121ca0304d92f5
Daniel Jiménez <danielji@student.42madrid.com>
2025-12-01 13:05:47 +0100

    Removed comment

--- libft/free_strings_array.c
+++ libft/free_strings_array.c
@@ -3,17 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   free_strings_array.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: enrgil-p <enrgil-p@student.42madrid.c      +#+  +:+       +#+        */
+/*   By: danielji <danielji@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/09 12:53:24 by enrgil-p          #+#    #+#             */
-/*   Updated: 2025/11/09 23:02:09 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2025/12/01 13:01:20 by danielji         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/* To debug add to line 23:
-ft_printf("Free array to %s\n", array[i]);//debug */
 void	free_strings_array(char **array)
 {
 	size_t	i;
